c1d6492fa87e12fc2fc29353f5cc070a33ca32c2
â <>
2024-07-07 22:42:13 +0200

    Added the scanner and terminal funionality. Also fixed bugs

--- main.c
+++ main.c
@@ -11,6 +11,9 @@
 
 sem_t cartSemaphore;
 sem_t terminalSemaphore;
+sem_t terminal_Semaphores[NUM_TERMINAL];
+sem_t terminal_queue_mutex[NUM_TERMINAL];
+sem_t terminal_ready[NUM_TERMINAL];
 sem_t cashiers_Semaphores[NUM_CASHIERS];
 sem_t cashier_queue_mutex[NUM_CASHIERS];
 sem_t cashier_ready[NUM_CASHIERS];
@@ -24,6 +27,7 @@ void *customer(void *arg);
 void *customerScanner(void *arg);
 void shopping(int customer_id);
 void checkout(int customer_id, int chosen_cashier);
+void checkoutScanner(int customer_id, int chosen_cashier);
 void return_cart(int customer_id);
 
 
@@ -35,57 +39,64 @@ void *customerScanner(void *arg) {
     sem_wait(&terminalSemaphore);
     if (scannersAvailable > 0) {
         scannersAvailable--;
-        printf("Customer %d takes a handheld scanner. Handheld scanners available: %d\n", customer_id, scannersAvailable);
+        printf("Customer [%d] takes a handheld scanner. Handheld scanners available: %d\n", customer_id, scannersAvailable);
         sem_post(&terminalSemaphore);
 
         // Customer starts shopping
         shopping(customer_id);
 
         // After shopping, proceed to checkout
-        int chosen_cashier = 0;
+        int chosen_terminal = 0;
 
-        // Choose the cashier with the smallest or no queue
-        for (int i = 1; i < NUM_CASHIERS; i++) {
-            sem_wait(&cashiers_Semaphores[i]); // Wait on semaphore for each cashier
+        // Choose the terminal with the no queue
+        for (int i = 1; i < NUM_TERMINAL; i++) {
+            sem_wait(&terminal_Semaphores[i]); // Wait on semaphore for each cashier
 
             // Check if current cashier's queue is smaller or non-existent
-            if (cashier_queue[i] < cashier_queue[chosen_cashier]) {
-                chosen_cashier = i;
+            if (cashier_queue[i] < cashier_queue[chosen_terminal]) {
+                chosen_terminal = i;
             }
+            sem_post(&terminal_Semaphores[i]); // Release semaphore for each cashier
+        }
+        int queue = cashier_queue[0] + cashier_queue[1];
 
-            sem_post(&cashiers_Semaphores[i]); // Release semaphore for each cashier
+        sem_wait(&terminal_queue_mutex[chosen_terminal]);
+
+        if(cashier_queue[chosen_terminal] == 0){
+            cashier_queue[chosen_terminal]++;
+            printf("Customer [%d] is checking out at terminal [%d].\n", customer_id, chosen_terminal);
+        } else{
+            printf("Customer [%d] has joined the queue. The current queue is [%d].\n", customer_id, queue - 1);
+            cashier_queue[chosen_terminal]++;
         }
 
-        // Join the chosen cashier's queue
-        sem_wait(&cashier_queue_mutex[chosen_cashier]);
-        cashier_queue[chosen_cashier]++;
-        printf("Customer [%d] is checking out at Cashier [%d] and the queue is %d.\n", customer_id, chosen_cashier, cashier_queue[chosen_cashier]);
-        sem_post(&cashier_queue_mutex[chosen_cashier]);
+
+        sem_post(&terminal_queue_mutex[chosen_terminal]);
 
         // Signal the chosen cashier that a customer is ready
         // sem_post(&cashier_ready[chosen_cashier]);
 
         // Wait for the chosen cashier to signal the customer's turn
-        sem_wait(&cashier_ready[chosen_cashier]);
+        sem_wait(&terminal_ready[chosen_terminal]);
 
         // Simulate checkout time
-        checkout(customer_id, chosen_cashier);
+        checkoutScanner(customer_id, chosen_terminal);
 
         // Customer finishes checkout, signal to the next customer in line
-        sem_post(&cashier_ready[chosen_cashier]);
+        sem_post(&terminal_ready[chosen_terminal]);
 
         // Update cashier queue
-        sem_wait(&cashier_queue_mutex[chosen_cashier]);
-        printf("Customer [%d] checking at Cashier [%d] has checked out.\n", customer_id, chosen_cashier);
-        cashier_queue[chosen_cashier]--;
-        sem_post(&cashier_queue_mutex[chosen_cashier]);
+        sem_wait(&terminal_queue_mutex[chosen_terminal]);
+        printf("Customer [%d] checking at terminal [%d] has checked out.\n", customer_id, chosen_terminal);
+        cashier_queue[chosen_terminal]--;
+        sem_post(&terminal_queue_mutex[chosen_terminal]);
 
-        // Return the cart and go home
-        return_cart(customer_id);
-    } else {
-        // No cart available, go home empty handed
-        sem_post(&cartSemaphore);
-        printf("Customer [%d] goes back home empty handed.\n", customer_id);
+        // Return the scanner and go home
+        sem_wait(&terminalSemaphore);
+        scannersAvailable++;
+        printf("Customer [%d] returned the scanner. Scanners available: %d\n", customer_id, scannersAvailable);
+        sem_post(&terminalSemaphore);
+
     }
 
     pthread_exit(NULL);
@@ -98,7 +109,7 @@ void *customer(void *arg) {
     sem_wait(&cartSemaphore);
     if (carts_available > 0) {
         carts_available--;
-        printf("Customer %d takes a cart. Carts available: %d\n", customer_id, carts_available);
+        printf("Customer [%d] takes a cart. Carts available: %d\n", customer_id, carts_available);
         sem_post(&cartSemaphore);
 
         // Customer starts shopping
@@ -122,7 +133,7 @@ void *customer(void *arg) {
         // Join the chosen cashier's queue
         sem_wait(&cashier_queue_mutex[chosen_cashier]);
         cashier_queue[chosen_cashier]++;
-        printf("Customer [%d] is checking out at Cashier [%d] and the queue is %d.\n", customer_id, chosen_cashier, cashier_queue[chosen_cashier]);
+        printf("Customer [%d] is checking out at Cashier [%d] and the queue is %d.\n", customer_id, chosen_cashier, cashier_queue[chosen_cashier] - 1);
         sem_post(&cashier_queue_mutex[chosen_cashier]);
 
         // Signal the chosen cashier that a customer is ready
@@ -167,6 +178,12 @@ void checkout(int customer_id, int chosen_cashier) {
     printf("Customer [%d] is checking out at Cashier [%d] for %d seconds.\n", customer_id, chosen_cashier, checkout_time);
     sleep(checkout_time);
 }
+void checkoutScanner(int customer_id, int chosen_cashier) {
+    int checkout_time = (rand() % 4) + 1;
+    printf("Customer [%d] is checking out at Terminal [%d] for %d seconds.\n", customer_id, chosen_cashier, checkout_time);
+    sleep(checkout_time);
+}
+
 
 void return_cart(int customer_id) {
     int return_time = (rand() % 2) + 1; // Random return time between 1 to 2 seconds
@@ -182,12 +199,19 @@ int main() {
     pthread_t customers[NUM_CUSTOMERS];
     int customer_ids[NUM_CUSTOMERS];
 
-    sem_init(&terminalSemaphore, 0, 1);
+
     sem_init(&cartSemaphore, 0, 1);
     for (int i = 0; i < NUM_CASHIERS; i++) {
         sem_init(&cashiers_Semaphores[i], 0, 1);
         sem_init(&cashier_queue_mutex[i], 0, 1);
         sem_init(&cashier_ready[i], 0, 1);
+    }
+
+    sem_init(&terminalSemaphore, 0, 1);
+    for (int i = 0; i < NUM_CASHIERS; i++) {
+        sem_init(&terminal_Semaphores[i], 0, 1);
+        sem_init(&terminal_queue_mutex[i], 0, 1);
+        sem_init(&terminal_ready[i], 0, 1);
     }
 
     srand(time(NULL));
@@ -200,28 +224,36 @@ int main() {
             usleep((rand() % 3 + 2) * 1000000); // Random interval between 2 to 4 seconds for customer arrival
         } else {
             if (scannersAvailable > 0) {
-                pthread_create(&customers[i], NULL, customer, &customer_ids[i]);
+                pthread_create(&customers[i], NULL, customerScanner, &customer_ids[i]);
                 usleep((rand() % 3 + 2) * 1000000); // Random interval between 2 to 4 seconds for customer arrival
-
             } else {
                 printf("No handheld scanners available. Customer [%d] will go for a cart.\n", customer_ids[i]);
-                pthread_create(&customers[i], NULL, customerScanner, &customer_ids[i]);
+                pthread_create(&customers[i], NULL, customer, &customer_ids[i]);
                 usleep((rand() % 3 + 2) * 1000000); // Random interval between 2 to 4 seconds for customer arrival
             }
 
         }
 
-        for (int i = 0; i < NUM_CUSTOMERS; i++) {
-            pthread_join(customers[i], NULL);
-        }
 
-        sem_destroy(&cartSemaphore);
-        for (int i = 0; i < NUM_CASHIERS; i++) {
-            sem_destroy(&cashiers_Semaphores[i]);
-            sem_destroy(&cashier_queue_mutex[i]);
-            sem_destroy(&cashier_ready[i]);
-        }
-
-        return 0;
     }
+
+    for (int i = 0; i < NUM_CUSTOMERS; i++) {
+        pthread_join(customers[i], NULL);
+    }
+
+    sem_destroy(&cartSemaphore);
+    for (int i = 0; i < NUM_CASHIERS; i++) {
+        sem_destroy(&cashiers_Semaphores[i]);
+        sem_destroy(&cashier_queue_mutex[i]);
+        sem_destroy(&cashier_ready[i]);
+    }
+
+    sem_destroy(&terminalSemaphore);
+    for (int i = 0; i < NUM_CASHIERS; i++) {
+        sem_destroy(&terminal_Semaphores[i]);
+        sem_destroy(&terminal_queue_mutex[i]);
+        sem_destroy(&terminal_ready[i]);
+    }
+
+    return 0;
 }
